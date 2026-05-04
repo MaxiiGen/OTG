b51c0153761d43333b30eeb8ba673458a34c9c3b
Glen Laurence Lagata <glenlaurencelagata@gmail.com>
2026-05-04 12:12:58 +0800

    Deploy PWA to GitHub Pages

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: process.env.VITE_BASE_PATH || '/OTG/',
   plugins: [
     inspectAttr(),
     react(),
